8eab7cdad442d45966d8939efac60e0cdac71620
Jake Harris-Liston <jake.harrisliston@gmail.com>
2025-12-26 21:44:15 +1000

    feat: cal embed added

--- frontend/src/components/MeetingModal/MeetingModal.jsx
+++ frontend/src/components/MeetingModal/MeetingModal.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "./meeting-modal.css"
-import { api } from "../../services/api";
+import Cal from "@calcom/embed-react";
+
 
 export default function MeetingModal ({ onClose }) {
 
+
     return(
-        <div className="meeting-modal-wrap">
-            <div className="meeting-modal">
+        <div className="meeting-modal-wrap" onClick={onClose}>
+            <div className="meeting-modal" onClick={(e) => e.stopPropagation()}>
                 <h1 className="modal-title">Book your slot</h1>
+                
                 <button 
                 onClick={onClose}
                 >Close</button>
+
+                <Cal calLink="jake-harris-liston-m0fd0d/30min" />
 
             </div>
 
